47e93933af5f69bd1b9ecf4e990ae13ab50ab488
vinayaachar <vinayaachar@gmail.com>
2021-05-29 13:38:11 -0700

    Complete Basic Minimum Requirement Challenge 2

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,110 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import axios from 'axios';
+import ChartJS from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
 
+export default class Crypto extends React.Component {
+  // chartRef = React.createRef();
+  constructor(props) {
+    super(props);
+    this.state = {
+      currencies: [],
+      usdRates: [],
+      gbpRates: [],
+      euRates: [],
+      dates: [],
+      chartData: {}
+    }
+
+    this.charts = this.charts.bind(this);
+  }
+
+
+  componentDidMount() {
+
+    // const myChartRef = this.chartRef.current.getContext("2d");
+
+    axios.get('http://localhost:3000/api')
+      .then(response => {
+        console.log(response.data);
+        var obj ={};
+        this.setState({
+          // currencies: response.data.currencies,
+          usdRates: response.data.usdRates,
+          dates: response.data.dates,
+          // gbpRates: response.data.gbpRates,
+          // euRates: response.data.euRates,
+        }, () => {
+          // this.charts(myChartRef);
+          console.log(this.state.usdRates);
+          obj = {
+            labels: this.state.dates,
+            datasets: [
+              {
+                label: 'USD/BTC',
+                data: this.state.usdRates,
+                fill: true,
+                backgroundColor: "rgba(75,192,192,0.2)",
+                borderColor: "rgba(75,192,192,1)"
+              },
+              // {
+              //   label: this.state.currencies[1],
+              //   data: this.state.gbpRates,
+              //   fill: true,
+              //   backgroundColor: "rgba(75,192,192,0.2)",
+              //   borderColor: "#742774"
+              // },
+              // {
+              //   label: this.state.currencies[2],
+              //   data: this.state.euRates,
+              //   fill: true,
+              //   backgroundColor: "rgba(75,192,192,0.2)",
+              //   borderColor: "#BF3F3F"
+              // },
+            ]
+          }
+          // var chart = new ChartJS(myChartRef, obj);
+        })
+        return obj;
+      })
+      .then(obj => {
+        console.log(obj)
+        this.setState({
+          chartData: obj
+        }, () => console.log(this.state.chartData))
+      })
+      .catch(err => console.log(err));
+  }
+
+  charts(myChartRef) {
+
+    console.log(myChartRef);
+    var obj = {
+      type: "line",
+      data: {
+        labels: this.state.currencies,
+        datasets: [
+          {
+            label: "BTC",
+            data: this.state.rates,
+          }
+        ]
+      }
+    }
+    var chart = new Chart(myChartRef, obj);
+    return chart;
+  }
+
+  render() {
+    return (
+      <div>
+        <Line data = {this.state.chartData} />
+      </div>
+    )
+  }
+}
+
+
+ReactDom.render(<Crypto />, document.getElementById('root'));
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,70 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+var cors = require('cors');
+const axios = require('axios');
 
 
+
+const port = 3000;
+
+var bodyParser = require('body-parser')
+
+app.use(express.static(__dirname + '/client/dist'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+//route
+var currencies = ['USD', 'GBP', 'EUR'];
+var usdRates = [];
+var euRates = [];
+var gbpRates = [];
+var obj = {};
+var dates = [];
+
+app.use('/api', (req, res) => {
+  getData()
+    .then(() => {
+      obj = {
+        usdRates: usdRates,
+        dates: dates
+      }
+      res.status(200).json(obj);
+    })
+})
+
+const loop = async () => {
+  for (var i = 0; i < 10 ; i++) {
+   await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(response => {
+      // rates.push(response.data.bpi.USD.rate, response.data.bpi.GBP.rate, response.data.bpi.EUR.rate);
+      usdRates.push(response.data.bpi.USD.rate_float);
+      euRates.push(response.data.bpi.EUR.rate_float);
+      gbpRates.push(response.data.bpi.GBP.rate_float);
+    })
+    .catch(err => console.log(err));
+  }
+}
+
+const getData = async () => {
+  await axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2020-05-05&end=2021-05-15')
+    .then(response => {
+      var data = response.data.bpi
+      for (var keys in data) {
+        dates.push(keys);
+        usdRates.push(data[keys]);
+      }
+    })
+    .catch(err => console.log(err));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: path.join(__dirname, './public')});
+})
+
+
+app.listen(port, () => {
+  console.log(`Mini Apps 2 Challenge 2 listening at http://localhost:${port}`);
+})
